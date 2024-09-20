7c9c3823678af8f1ae7289e154c39e4c796fafff
Ajala Oluwafemi Daniel <146761013+oluwafemidan@users.noreply.github.com>
2024-09-20 23:27:42 +0100

    Update qachat.py

--- qachat.py
+++ qachat.py
@@ -1,5 +1,5 @@
-from dotenv import load_dotenv
-load_dotenv()  # Load environment variables from .env file
+#from dotenv import load_dotenv
+#load_dotenv()  # Load environment variables from .env file
 
 import streamlit as st
 import os
